9a48d72f1de64b354a8805533f5fc582e6cc43dd
Mayank <mayankjha632004@gmail.com>
2025-07-19 00:04:56 +0530

    Origins fixed

--- hospital_api.py
+++ hospital_api.py
@@ -51,7 +51,6 @@ def predict():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    
     port = int(os.environ.get("PORT", 5000))  # 5000 is fallback if not running on Render
 app.run(host='0.0.0.0', port=port)
 
